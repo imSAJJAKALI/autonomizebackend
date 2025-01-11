0d72b01b86b1815f74330f610c2c7101bed87df2
SAJJAK ALI <112471219+imSAJJAKALI@users.noreply.github.com>
2025-01-11 09:30:03 +0530

    Update githubApi.js

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,8 +1,9 @@
-import axios from "axios";
-
 const fetchGitHubUser = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
+  const token = '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'
+  const response = await axios.get(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
-
-export default fetchGitHubUser;
