2898616496891f94e3ac5c2e524f5e3fcacb637a
imSAJJAKALI <sajjak2506@gmail.com>
2025-01-11 09:53:22 +0530

    axios

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 const fetchGitHubUser = async (username) => {
   const token = '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'
   const response = await axios.get(`https://api.github.com/users/${username}`, {
