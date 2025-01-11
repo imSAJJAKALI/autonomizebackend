fbd72a2d237927a6236db9fbd193208244d73cbf
imSAJJAKALI <sajjak2506@gmail.com>
2025-01-11 10:04:42 +0530

    sss

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
-const fetchGitHubUser = async (username) => {
-  const token = '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'
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+import axios from 'axios';
 
-  return response.data;
+const fetchGitHubUser = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data; // Return the data from the response
+  } catch (error) {
+    console.error('Error fetching GitHub user:', error.message);
+    throw new Error('Unable to fetch GitHub user data');
+  }
 };
 
 export default fetchGitHubUser;
